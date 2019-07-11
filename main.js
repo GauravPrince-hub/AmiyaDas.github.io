b12b72282cb15a0c1f579fd714186f5c1819f48a
Amiya Ranjan Das <ranjandass13@gmail.com>
2019-07-11 14:14:00 +0530

    Final UI without service binding

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ".mat-toolbar.mat-primary {\r\n    background: #fff;\r\n    color: #111;\r\n    font-weight: bold;\r\n    box-shadow: 1px 1px 10px 1px #aaa;\r\n    z-index: 10;\r\n    position: fixed;\r\n}\r\n\r\n.mat-toolbar-row, .mat-toolbar-single-row {\r\n    height: 3rem;\r\n}\r\n\r\n.home-page {\r\n    width: 100%;\r\n    /* position: fixed; */\r\n}"
+module.exports = ".mat-toolbar.mat-primary {\r\n    background: #fff;\r\n    color: #111;\r\n    font-weight: bold;\r\n    box-shadow: 1px 1px 10px 1px #aaa;\r\n    z-index: 10;\r\n    position: fixed;\r\n}\r\n\r\n.mat-toolbar-row, .mat-toolbar-single-row {\r\n    height: 3rem;\r\n}"
 
 /***/ }),
 
@@ -41,7 +41,7 @@ module.exports = ".mat-toolbar.mat-primary {\r\n    background: #fff;\r\n    col
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<mat-toolbar color=\"primary\" role=\"heading\">\n  <span>\n    Device Dashboard\n  </span>\n  <span class=\"example-fill-remaining-space\"></span>\n  <span>\n    <button mat-icon-button>\n      <mat-icon>notifications_none</mat-icon>\n    </button>\n    <button mat-icon-button>\n      <mat-icon>settings</mat-icon>\n    </button>\n  </span>\n</mat-toolbar>\n<app-home class=\"home-page\"></app-home>"
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<mat-toolbar color=\"primary\" role=\"heading\">\n  <span>\n    Device Dashboard\n  </span>\n  <span class=\"example-fill-remaining-space\"></span>\n  <span>\n    <button mat-icon-button>\n      <mat-icon>notifications_none</mat-icon>\n    </button>\n    <button mat-icon-button>\n      <mat-icon>settings</mat-icon>\n    </button>\n  </span>\n</mat-toolbar>\n\n<app-home></app-home>"
 
 /***/ }),
 
@@ -99,14 +99,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _home_home_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./home/home.component */ "./src/app/home/home.component.ts");
 /* harmony import */ var _components_dashboard_card_dashboard_card_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/dashboard-card/dashboard-card.component */ "./src/app/components/dashboard-card/dashboard-card.component.ts");
 /* harmony import */ var _components_device_table_device_table_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/device-table/device-table.component */ "./src/app/components/device-table/device-table.component.ts");
-/* harmony import */ var _components_dashboard_chart_dashboard_chart_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/dashboard-chart/dashboard-chart.component */ "./src/app/components/dashboard-chart/dashboard-chart.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-
 
 
 
@@ -124,8 +122,7 @@ var AppModule = /** @class */ (function () {
                 _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"],
                 _home_home_component__WEBPACK_IMPORTED_MODULE_5__["HomeComponent"],
                 _components_dashboard_card_dashboard_card_component__WEBPACK_IMPORTED_MODULE_6__["DashboardCardComponent"],
-                _components_device_table_device_table_component__WEBPACK_IMPORTED_MODULE_7__["DeviceTableComponent"],
-                _components_dashboard_chart_dashboard_chart_component__WEBPACK_IMPORTED_MODULE_8__["DashboardChartComponent"]
+                _components_device_table_device_table_component__WEBPACK_IMPORTED_MODULE_7__["DeviceTableComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
@@ -165,7 +162,7 @@ module.exports = ".dashboard-card {\r\n    box-shadow: 1px 1px 10px 1px #aaa;\r\
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"dashboard-card\">\n  <div class=\"dashboard-card-header\">\n    {{cardTitle}}\n    <span class=\"dashboard-card-header-icon\">\n      <mat-icon [matMenuTriggerFor]=\"cardMenu\">more_horiz</mat-icon>\n    </span>\n    <mat-menu #cardMenu=\"matMenu\">\n      <button mat-menu-item>Item 1</button>\n      <button mat-menu-item>Item 2</button>\n    </mat-menu>\n  </div>\n  <ng-content></ng-content>\n</div>"
+module.exports = "<div class=\"dashboard-card\">\n  <div class=\"dashboard-card-header\">\n    {{cardTitle}}\n    <span class=\"dashboard-card-header-icon\">\n      <mat-icon>more_horiz</mat-icon>\n    </span>\n  </div>\n  <ng-content></ng-content>\n</div>"
 
 /***/ }),
 
@@ -214,114 +211,6 @@ var DashboardCardComponent = /** @class */ (function () {
 
 /***/ }),
 
-/***/ "./src/app/components/dashboard-chart/dashboard-chart.component.css":
-/*!**************************************************************************!*\
-  !*** ./src/app/components/dashboard-chart/dashboard-chart.component.css ***!
-  \**************************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-module.exports = ".chart-alignemnet {\r\n    text-align: center;\r\n}"
-
-/***/ }),
-
-/***/ "./src/app/components/dashboard-chart/dashboard-chart.component.html":
-/*!***************************************************************************!*\
-  !*** ./src/app/components/dashboard-chart/dashboard-chart.component.html ***!
-  \***************************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-module.exports = "<div id=\"dashboard_chart\" class=\"chart-alignemnet\">\n</div>"
-
-/***/ }),
-
-/***/ "./src/app/components/dashboard-chart/dashboard-chart.component.ts":
-/*!*************************************************************************!*\
-  !*** ./src/app/components/dashboard-chart/dashboard-chart.component.ts ***!
-  \*************************************************************************/
-/*! exports provided: DashboardChartComponent */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DashboardChartComponent", function() { return DashboardChartComponent; });
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
-/* harmony import */ var d3__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! d3 */ "./node_modules/d3/d3.js");
-/* harmony import */ var d3__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(d3__WEBPACK_IMPORTED_MODULE_1__);
-var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (undefined && undefined.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-
-
-// import {scaleLinear} from "d3-scale";
-var DashboardChartComponent = /** @class */ (function () {
-    function DashboardChartComponent() {
-    }
-    DashboardChartComponent.prototype.ngOnInit = function () {
-        this.constructD3Chart();
-    };
-    DashboardChartComponent.prototype.constructD3Chart = function () {
-        var data = [{ "name": "Dev", "value": 53 }, { "name": "PO", "value": 15 }, { "name": "QA", "value": 32 }];
-        var margin = { top: 10, right: 10, bottom: 30, left: 10 }, width = 200 - margin.left - margin.right, height = 150 - margin.top - margin.bottom;
-        var x = d3__WEBPACK_IMPORTED_MODULE_1__["scale"].ordinal().rangeRoundBands([0, width], .05);
-        var y = d3__WEBPACK_IMPORTED_MODULE_1__["scale"].linear().range([height, 0]);
-        var xAxis = d3__WEBPACK_IMPORTED_MODULE_1__["svg"].axis()
-            .scale(x);
-        // .orient("bottom");
-        var svg = d3__WEBPACK_IMPORTED_MODULE_1__["select"]("#dashboard_chart").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        x.domain(data.map(function (d) { return d.name; }));
-        y.domain([0, d3__WEBPACK_IMPORTED_MODULE_1__["max"](data, function (d) { return d.value; })]);
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "1em")
-            .attr("dy", "1em");
-        svg.selectAll("bar")
-            .data(data)
-            .enter().append("rect")
-            .style("fill", "#525")
-            .attr("x", function (d) { return 5 + x(d.name); })
-            .attr("width", "20%") //x.rangeBand())
-            .attr("y", function (d) { return y(d.value); })
-            .attr("height", function (d) { return height - y(d.value); });
-        svg.selectAll("bar")
-            .data(data)
-            .enter().append("text")
-            .attr('x', function (d) { return 25 + x(d.name); })
-            .attr('y', height - 10)
-            .attr('text-anchor', 'middle')
-            .attr("class", "label")
-            .text('21%');
-    };
-    DashboardChartComponent = __decorate([
-        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
-            selector: 'app-dashboard-chart',
-            template: __webpack_require__(/*! ./dashboard-chart.component.html */ "./src/app/components/dashboard-chart/dashboard-chart.component.html"),
-            styles: [__webpack_require__(/*! ./dashboard-chart.component.css */ "./src/app/components/dashboard-chart/dashboard-chart.component.css")]
-        }),
-        __metadata("design:paramtypes", [])
-    ], DashboardChartComponent);
-    return DashboardChartComponent;
-}());
-
-
-
-/***/ }),
-
 /***/ "./src/app/components/device-table/device-table.component.css":
 /*!********************************************************************!*\
   !*** ./src/app/components/device-table/device-table.component.css ***!
@@ -340,7 +229,7 @@ module.exports = ".device-table {\r\n    box-shadow: 1px 1px 10px 1px #aaa;\r\n 
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"device-table\">\n  <div class=\"device-table-header\">\n    Device List\n    <span class=\"device-table-header-icon\">\n      <mat-icon>more_horiz</mat-icon>\n    </span>\n  </div>\n  <div class=\"table-responsive\">\n  <table class=\"table table-striped\">\n    <thead>\n      <tr>\n        <th scope=\"col\">No.</th>\n        <th scope=\"col\">User name</th>\n        <th scope=\"col\">User role</th>\n        <th scope=\"col\">Location</th>\n        <th scope=\"col\">Status</th>\n        <th scope=\"col\">Device</th>\n        <th scope=\"col\">Version no.</th>\n        <th scope=\"col\">Battery</th>\n      </tr>\n    </thead>\n    <tbody>\n      <tr>\n        <th scope=\"row\">1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n        <th>1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n      </tr>\n      <tr>\n        <th scope=\"row\">2</th>\n        <td>Jacob</td>\n        <td>Thornton</td>\n        <td>fat</td>\n        <th>1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n      </tr>\n      <tr>\n        <th scope=\"row\">3</th>\n        <td>Larry</td>\n        <td>the Bird</td>\n        <td>twitter</td>\n        <th>1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n      </tr>\n    </tbody>\n  </table>\n</div>\n</div>"
+module.exports = "<div class=\"device-table\">\n  <div class=\"device-table-header\">\n    Device List\n    <span class=\"device-table-header-icon\">\n      <mat-icon>more_horiz</mat-icon>\n    </span>\n    <!-- No. User name User role Location Status Device Version no. Battery  -->\n  </div>\n  <table class=\"table table-striped\">\n    <thead>\n      <tr>\n        <th scope=\"col\">No.</th>\n        <th scope=\"col\">User name</th>\n        <th scope=\"col\">User role</th>\n        <th scope=\"col\">Location</th>\n        <th scope=\"col\">Status</th>\n        <th scope=\"col\">Device</th>\n        <th scope=\"col\">Version no.</th>\n        <th scope=\"col\">Battery</th>\n      </tr>\n    </thead>\n    <tbody>\n      <tr>\n        <th scope=\"row\">1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n        <th>1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n      </tr>\n      <tr>\n        <th scope=\"row\">2</th>\n        <td>Jacob</td>\n        <td>Thornton</td>\n        <td>fat</td>\n        <th>1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n      </tr>\n      <tr>\n        <th scope=\"row\">3</th>\n        <td>Larry</td>\n        <td>the Bird</td>\n        <td>twitter</td>\n        <th>1</th>\n        <td>Mark</td>\n        <td>Otto</td>\n        <td>mdo</td>\n      </tr>\n    </tbody>\n  </table>\n</div>"
 
 /***/ }),
 
@@ -392,7 +281,7 @@ var DeviceTableComponent = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ".example-fill-remaining-space {\r\n    /* This fills the remaining space, by using flexbox. \r\n       Every toolbar row uses a flexbox row layout. */\r\n    -webkit-box-flex: 1;\r\n            flex: 1 1 auto;\r\n}\r\n\r\n.title-text {\r\n    font-family: Arial, Helvetica, sans-serif;\r\n    font-size: 18pt;\r\n    margin-bottom: 1rem;\r\n}\r\n\r\n.dropdown-button-wrapper {\r\n    border: 1.5px solid #aaa;\r\n    border-radius: 8px;\r\n    padding: 0.3rem;\r\n    float: right;\r\n}\r\n\r\n.home-padding {\r\n    padding: 2rem;\r\n    padding-top: 4rem;\r\n}\r\n\r\n.card-block {\r\n    margin-top: 1.5rem;\r\n}"
+module.exports = ".example-fill-remaining-space {\r\n    /* This fills the remaining space, by using flexbox. \r\n       Every toolbar row uses a flexbox row layout. */\r\n    -webkit-box-flex: 1;\r\n            flex: 1 1 auto;\r\n}\r\n\r\n.title-text {\r\n    font-family: Arial, Helvetica, sans-serif;\r\n    font-size: 18pt;\r\n    margin-bottom: 1rem;\r\n}\r\n\r\n.dropdown-button-wrapper {\r\n    border: 1.5px solid #aaa;\r\n    border-radius: 8px;\r\n    padding: 0.3rem;\r\n    float: right;\r\n}\r\n\r\n.home-padding {\r\n    padding: 1.5rem;\r\n    padding-top: 4rem;\r\n}\r\n\r\n.card-block {\r\n    margin-top: 1.5rem;\r\n}"
 
 /***/ }),
 
@@ -403,7 +292,7 @@ module.exports = ".example-fill-remaining-space {\r\n    /* This fills the remai
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"home-padding\">\n  <div>\n    <span class=\"title-text\">Real-time Overview</span>\n    <span class=\"dropdown\">\n      <button [matMenuTriggerFor]=\"menu\" class=\"btn dropdown-toggle dropdown-button-wrapper\">Export\n      </button>\n      <mat-menu #menu=\"matMenu\">\n        <button mat-menu-item>Item 1</button>\n        <button mat-menu-item>Item 2</button>\n      </mat-menu>\n    </span>\n  </div>\n\n  <div class=\"row card-block\">\n    <app-dashboard-card cardTitle=\"Active Devices\" class=\"col-md-4 col-sm-12 col-xs-12 col-12\">text to insert\n    </app-dashboard-card>\n    <app-dashboard-card cardTitle=\"Roles\" class=\"col-md-4 col-sm-12 col-xs-12 col-12\">\n      <app-dashboard-chart></app-dashboard-chart>\n    </app-dashboard-card>\n    <app-dashboard-card cardTitle=\"Sample Board\" class=\"col-md-4 col-sm-12 col-xs-12 col-12\"></app-dashboard-card>\n  </div>\n\n  <app-device-table></app-device-table>\n</div>"
+module.exports = "<div class=\"home-padding\">\n  <div>\n    <span class=\"title-text\">Real-time Overview</span>\n    <span class=\"dropdown\">\n        <button [matMenuTriggerFor]=\"menu\" class=\"btn dropdown-toggle dropdown-button-wrapper\">Export\n        </button>\n        <mat-menu #menu=\"matMenu\">\n          <button mat-menu-item>Item 1</button>\n          <button mat-menu-item>Item 2</button>\n        </mat-menu>\n      </span>\n  </div>\n\n  <div class=\"row card-block\">\n    <app-dashboard-card cardTitle=\"Active Devices\" class=\"col-md-4 col-sm-12 col-xs-12\">text to insert</app-dashboard-card>\n    <app-dashboard-card cardTitle=\"Roles\" class=\"col-md-4 col-sm-12 col-xs-12\">\n      <div>\n        text\n      </div>\n    </app-dashboard-card>\n    <app-dashboard-card cardTitle=\"Sample Board\" class=\"col-md-4 col-sm-12 col-xs-12\"></app-dashboard-card>\n\n  </div>\n\n  <app-device-table>\n\n  </app-device-table>\n</div>"
 
 /***/ }),
 
